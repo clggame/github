fe5ff099e2cbf3e345ed356e360f730f701e30a7
clggame <59595512@qq.com>
2015-12-05 14:54:25 +0800

    第一个版本
    第一个版本

---
+++ Fly3/Assets/Script/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+[AddComponentMenu("MYGame/Player")]
+
+public class Player : MonoBehaviour {
+	
+	// 移动速度
+	public float m_speed = 10;
+	
+	protected Transform m_transform;
+
+	public Transform m_rocket;
+
+	// Use this for initialization
+	void Start () {
+		m_transform = this.transform;
+	}
+	 
+	// Update is called once per frame
+	void Update () {
+		
+		// 纵向移动距离
+		float movev = 0;
+		
+		// 水平移动距离
+		float moveh = 0;
+		
+		// 按上键
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			movev -= m_speed * Time.deltaTime;
+		}
+		
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			movev += m_speed * Time.deltaTime;		
+		}
+		
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			moveh += m_speed * Time.deltaTime;
+		}
+		
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			moveh -= m_speed * Time.deltaTime;
+		}
+		
+		this.m_transform.Translate (new Vector3 (moveh, 0, movev));
+
+		if (Input.GetKey (KeyCode.Space)) {
+			Instantiate(m_rocket, m_transform.position, m_transform.rotation);
+		}
+		
+	}
+}
